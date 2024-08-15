9d8088a416257f3fe381acb75ce683077bc617e1
Alison Emilien <aemil072@uottawa.ca>
2024-08-14 23:06:49 -0400

    Added svg logo files

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,21 +1,31 @@
 import SideBarIcon from "./SideBarIcon";
-import img1 from "./assets/react.svg";
+import img0 from "./assets/react.svg";
+import img1 from "./assets/House.svg";
+import img2 from "./assets/Skills.svg";
+import img3 from "./assets/Project.svg";
+import img4 from "./assets/Experience.svg";
+import img5 from "./assets/Contact.svg";
 
 
-const SideBar = () => {
+const SideBar = () => { 
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-mytext shadow-lg">
-            {/* Home and About*/}
-            <SideBarIcon icon={img1} hyperlink="https://www.youtube.com/watch?v=xvFZjo5PgG0"/>
-            {/* Skills and Knowledge */}
-            <i>B</i>
-            {/* Projects */}
-            <i>C</i>
-            {/* Experiences */}
-            <i>D</i>
-            {/* Contact */}
-            <i>E</i>
-        </div>
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 justify-between flex flex-col bg-primary text-mytext shadow-lg">
+                <div>
+                <SideBarIcon icon={img0} hyperlink="https://www.youtube.com/watch?v=xvFZjo5PgG0"/>
+                </div>
+                <div className="flex flex-col gap-5">
+                    {/* Home and About*/}
+                    <SideBarIcon icon={img1} hyperlink="https://www.youtube.com/watch?v=xvFZjo5PgG0" title="Home"/>
+                    {/* Skills and Knowledge */}
+                    <SideBarIcon icon={img2} hyperlink="https://www.youtube.com/watch?v=xvFZjo5PgG0"/>
+                    {/* Projects */}
+                    <SideBarIcon icon={img3} hyperlink="https://www.youtube.com/watch?v=xvFZjo5PgG0"/>
+                    {/* Experiences */}
+                    <SideBarIcon icon={img4} hyperlink="https://www.youtube.com/watch?v=xvFZjo5PgG0"/>
+                    {/* Contact */}
+                    <SideBarIcon icon={img5} hyperlink="https://www.youtube.com/watch?v=xvFZjo5PgG0"/>
+                </div>
+            </div>
     );
 };  
 export default SideBar;
--- src/components/SideBarIcon.jsx
+++ src/components/SideBarIcon.jsx
@@ -1,8 +1,8 @@
-const SideBarIcon = ({ icon,hyperlink }) => {
+const SideBarIcon = ({ icon,hyperlink,title }) => {
   return (
-    <div className="w-10 h-10 m-4"> 
+    <div className="w-10 h-10 m-4" title={title}> 
         <a href={hyperlink}> 
-            <img src={icon} alt="icon"/>
+            <img className="iconStyle" src={icon} alt="icon"/>
         </a>
     </div>
   );
