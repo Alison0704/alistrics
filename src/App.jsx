f4baef12022f7ea6852415d1068b62a54547cc78
Alison Emilien <aemil072@uottawa.ca>
2024-08-22 19:28:28 -0400

    removing Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useDebugValue, useState } from 'react'
 import './App.css'
 import SideBar from './components/SideBar'
 import Section from './components/Section';
-import Home from './components/Sections/Home'
-
 function App() {
   return (
     <div className="flex">
       <SideBar />
       <div className="flex-row">
-      <Section children={Home}/>
+      <Section/>
       </div>
     </div>
   );
