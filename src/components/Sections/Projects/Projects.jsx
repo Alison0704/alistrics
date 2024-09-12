45bb7f4f9800e3317de5cccc9866b66284f3c4e7
Alison Emilien <aemil072@uottawa.ca>
2024-09-12 19:43:51 -0400

    minor fixes and removed images in home section

--- src/components/Sections/Projects/Projects.jsx
+++ src/components/Sections/Projects/Projects.jsx
@@ -1,10 +1,10 @@
 function Projects() {
   return (
-    <section className="flex flex-row relative w-fit h-fit p-6 gap-4 content-center">
-        <p className="text-mainColor30 content-center text-7xl">Projects</p>
-        <div className="flex flex-row p-16 gap-6 bg-mainColor30 bg-opacity-15 rounded-3xl w-full">   
+    <section className="flex flex-col justify-center">
+        <p className="text-mainColor30 content-center text-7xl text-center">Projects</p>
+        <div className="flex flex-col p-16 gap-6 bg-mainColor30 bg-opacity-15 rounded-3xl w-full">   
              <p className="text-mainColor30">Here, you'll find a collection of my personal and professional endeavors. From passion projects to collaborative work, each project represents a unique learning experience and a chance to explore new ideas.</p>
-             <button className="bg-transparent border-2 border-mainColor10 text-mainColor10 rounded-xl p-2">Projects Page</button>
+             <button className="bg-transparent border-2 border-mainColor10 text-mainColor10 rounded-xl text-xl p-2">Projects Page</button>
         </div>
     </section>
   );
