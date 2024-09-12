2ebfaa72667f1e788bcd9e4806f645ef0894d717
Alison Emilien <aemil072@uottawa.ca>
2024-09-12 19:52:32 -0400

    from MyHome to myHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 import SideBar from './components/SideBar';
-import MyHome from './components/Sections/MyHome';
+import MyHome from './components/Sections/myHome';
 import Skills from './components/Sections/Skills/Skills';
 import Experiences from './components/Sections/Experience/Experience';
 import Contact from './components/Sections/Contact';
