a5ac87bb1cf76e6e83f5f74ed035883372df9119
Alison Emilien <aemil072@uottawa.ca>
2024-09-16 18:47:30 -0400

    Fixed email issue

--- src/components/ContactIcon.jsx
+++ src/components/ContactIcon.jsx
@@ -1,11 +1,10 @@
-import AnchorLink from "react-anchor-link-smooth-scroll";
-function SideBarIcon({ icon,anchorlink,link,title}){
+function ContactIcon ({icon, title,link}){
   return (
    
     <div className="relative group w-14 h-14 m-4 cursor-pointer flex flex-row" title={title}>
-        <AnchorLink href={anchorlink}> 
+        <a href={link}> 
         <img src={icon} alt="icon"/>
-        </AnchorLink>
+        </a>
       <span className="absolute left-20 transform w-auto p-2 m-2 min-w-max rounded-xl shadow-md text-mainColor30 bg-mainColor60 border-mainColor30 border-2 text-s font-bold transition-opacity duration-200 opacity-0 group-hover:opacity-100">
         {title}
       </span>
@@ -14,4 +13,4 @@ function SideBarIcon({ icon,anchorlink,link,title}){
    
   );
 }
-export default SideBarIcon;
+export default ContactIcon;
