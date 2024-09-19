414461116a41b5b2c1e075b40ceae24b6564a051
Alison Emilien <aemil072@uottawa.ca>
2024-09-19 19:23:19 -0400

    background added with project page

--- src/components/Sections/Projects/Projects.jsx
+++ src/components/Sections/Projects/Projects.jsx
@@ -4,7 +4,14 @@ function Projects() {
         <p className="text-mainColor10 content-center text-8xl text-center pb-7">Projects</p>
         <div className="flex flex-col p-16 gap-6 bg-mainColor30 bg-opacity-15 rounded-3xl w-full">   
              <p className="text-mainColor30">Here, you'll find a collection of my personal and professional endeavors. From passion projects to collaborative work, each project represents a unique learning experience and a chance to explore new ideas.</p>
-             <button className="bg-transparent border-2 border-mainColor10 text-mainColor10 rounded-xl text-xl p-2">Projects Page</button>
+             <button
+               className="bg-transparent border-2 border-mainColor10 text-mainColor10 rounded-xl text-xl p-2"
+               onClick={() => window.location.href = 'src/components/Sections/Projects/project.html'}
+               type="button"
+             >
+               View more
+             </button>
+            
         </div>
     </section>
   );
