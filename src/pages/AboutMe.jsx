96d50454bb4ebf4c843def7dada86a6b65b0e667
Alison Emilien <aemil072@uottawa.ca>
2025-02-10 02:56:49 -0500

    something really minor

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -6,7 +6,7 @@ function AboutMe({ setCurrentPage }) {
     <>
         <div className='aboutSection flex flex-col pl-60 pr-60'>
       <p className="text-orange-500">About Me</p>
-      <p className="text-orange-500">My name is Alison Emilien and I am a web developer.</p>
+      <p className="text-orange-500">My name is Alison Emilien</p>
       <button
         className="bg-transparent border-2 border-mainColor10 text-mainColor10 rounded-xl text-xl p-2 mt-4"
         type="button"
